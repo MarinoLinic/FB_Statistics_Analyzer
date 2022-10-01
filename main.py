77a3a6c3379f422c00b6e163ed9692e0c1fc704f
MarinoLinic <84214511+MarinoLinic@users.noreply.github.com>
2022-10-01 17:02:48 +0200

    Terrible update

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import json
 import pathlib
 import plotly.express as px
 import plotly.graph_objects as go
+import numpy as np
 
 
 def correct_name(arr):
     new_arr = []
     for user in arr:
-        new_arr.append(user.replace("Ä", "ć"))
+        user = user.replace("Ä", "ć")
+        user = user.replace("Ä", "č")
+        user = user.replace("Å ", "Š")
+        user = user.replace("Å¡", "š")
+        user = user.replace("Å½", "Ž")
+        user = user.replace("Å¾", "ž")
+        new_arr.append(user)
     return new_arr
 
 
@@ -55,27 +62,68 @@ def users_msg(data, users):
     return user_arr, user_content
 
 
-def piechart(labels, values, name):
-    colors = ['gold', 'mediumturquoise', 'darkorange', 'lightgreen']
+def selection_sort(x, y, z):
+    for i in range(len(x)):
+        swap = i + np.argmin(x[i:])
+        (x[i], x[swap]) = (x[swap], x[i])
+        (y[i], y[swap]) = (y[swap], y[i])
+        (z[i], z[swap]) = (z[swap], z[i])
+    return x, y, z
+
+
+def revert_array(arr):
+    res = arr[::-1]
+    return res
+
+
+def reduce_array(arr):
+    new_arr = []
+    if len(arr) > 10:
+        other = 0
+        for j in range(0, 9):
+            new_arr.append(arr[j])
+        for j in range(10, len(arr)):
+            other += arr[j]
+        new_arr.append(other)
+    else:
+        new_arr = arr
+    return new_arr
+
+
+def piechart(labels, values, title):
+    colors = ['gold', 'mediumturquoise', 'darkorange', 'lightgreen', 'royalblue', ]
 
     fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
-    fig.update_traces(hoverinfo='label+percent', textinfo='value+percent', textfont_size=20,
+    fig.update_traces(hoverinfo='label+percent', textinfo='value+percent', textfont_size=15,
                       marker=dict(colors=colors, line=dict(color='#000000', width=2)))
-    fig.update_layout(title=name)
-    fig.write_image("Images/{}.png".format(name), scale=10)
+    fig.update_layout(title=title)
+    fig.write_image("Images/{}.png".format(title), scale=10)
 
 
-DATA_ADDRESS = "JSON_data"
+GROUP_NAME = "OI 2019-2020"
+DATA_ADDRESS = "Testing2"
 JSON_DATA = path_iter(DATA_ADDRESS)
 USERS = find_users(JSON_DATA)
 TOTAL_MSGS = all_msg(JSON_DATA)
 TOTAL_MSGS_PER_USER, USER_CONTENT = users_msg(JSON_DATA, USERS)
 
 USERS = correct_name(USERS)
+
+USER_CONTENT, USERS, TOTAL_MSGS_PER_USER = selection_sort(USER_CONTENT, USERS, TOTAL_MSGS_PER_USER)
+USERS = revert_array(USERS)
+USER_CONTENT = revert_array(USER_CONTENT)
+TOTAL_MSGS_PER_USER = revert_array(TOTAL_MSGS_PER_USER)
+
+TOTAL_MSGS_PER_USER = reduce_array(TOTAL_MSGS_PER_USER)
+USER_CONTENT = reduce_array(USER_CONTENT)
+
+for i in range(9, len(USERS)):
+    USERS.pop()
+USERS.append('Other')
+
 print(USERS)
 print(TOTAL_MSGS_PER_USER)
 print(USER_CONTENT)
 
-piechart(USERS, USER_CONTENT, "Total number of characters sent per user in The Bailey")
-piechart(USERS, TOTAL_MSGS_PER_USER, "Total number of messages sent per user in The Bailey")
-
+piechart(USERS, USER_CONTENT, "Total number of CHARACTERS sent per user in {}".format(GROUP_NAME))
+piechart(USERS, TOTAL_MSGS_PER_USER, "Total number of MESSAGES sent per user in {}".format(GROUP_NAME))
